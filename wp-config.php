aacdca210db7dc6fdb75d075ba515d22a13f45b4
element4git <elementcontrol@gmail.com>
2015-03-17 00:33:37 -0400

    updated config

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'bn_wordpress');
 define('DB_PASSWORD', '6301f23ab8');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
